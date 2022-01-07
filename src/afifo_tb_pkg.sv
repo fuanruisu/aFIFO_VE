c3e32c1b578b3af83e2061670865e86ef9c5e489
Juan Luis Magaña Paz <opomola@gmail.com>
2022-01-07 13:59:31 -0600

    Deleting old files

--- src/afifo_tb_pkg.sv
+++
@@ -1,7 +0,0 @@
-package afifo_tb_pkg;
-
-localparam DW = 16;
-typedef logic [DW-1] data_t;
-typedef bit bit_t;
-
-endpackage
--- src/tester_afifo.sv
+++
@@ -1,9 +0,0 @@
-class tester_afifo;
-
-virtual afifo_if itf;
-
-function new(virtual afifo_if.tstr t);
-	itf = t;
-endfunction
-
-endclass
